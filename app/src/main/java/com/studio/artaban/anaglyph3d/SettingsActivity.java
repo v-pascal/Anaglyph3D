5f20cd6beec272e57de30cb8eb24726c30378472
v-pascal <pascal.viguie@studio-artaban.com>
2016-05-04 09:36:20 +0200

    Files updated

--- app/src/main/java/com/studio/artaban/anaglyph3d/SettingsActivity.java
+++ app/src/main/java/com/studio/artaban/anaglyph3d/SettingsActivity.java
@@ -273,9 +273,9 @@ public class SettingsActivity extends SettingsParentActivity
     public boolean onMenuItemSelected(int featureId, MenuItem item) {
         int id = item.getItemId();
         if (id == android.R.id.home) {
-            if (!super.onMenuItemSelected(featureId, item)) {
+            if (!super.onMenuItemSelected(featureId, item))
                 NavUtils.navigateUpFromSameTask(this);
-            }
+
             return true;
         }
         return super.onMenuItemSelected(featureId, item);
