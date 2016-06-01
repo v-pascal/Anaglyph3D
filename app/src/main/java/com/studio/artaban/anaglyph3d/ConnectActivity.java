68fd372115f05aa7ebf9ff2a1da6c22a365a3818
v-pascal <pascal.viguie@studio-artaban.com>
2016-06-01 18:48:17 +0200

    Simulated 3D updates

--- app/src/main/java/com/studio/artaban/anaglyph3d/ConnectActivity.java
+++ app/src/main/java/com/studio/artaban/anaglyph3d/ConnectActivity.java
@@ -1,70 +1,30 @@
 package com.studio.artaban.anaglyph3d;
 
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.graphics.drawable.AnimationDrawable;
-import android.os.AsyncTask;
 import android.os.Build;
-import android.os.Parcelable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.TypedValue;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.studio.artaban.anaglyph3d.album.AlbumActivity;
-import com.studio.artaban.anaglyph3d.album.VideoListActivity;
-import com.studio.artaban.anaglyph3d.data.AlbumTable;
 import com.studio.artaban.anaglyph3d.data.Constants;
 import com.studio.artaban.anaglyph3d.helpers.ActivityWrapper;
-import com.studio.artaban.anaglyph3d.helpers.DisplayMessage;
-import com.studio.artaban.anaglyph3d.helpers.Internet;
 import com.studio.artaban.anaglyph3d.helpers.Logs;
-import com.studio.artaban.anaglyph3d.helpers.Storage;
 import com.studio.artaban.anaglyph3d.transfer.Connectivity;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.File;
-import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 /**
  * Created by pascal on 19/03/16.
  * Connect activity (launcher)
  */
 public class ConnectActivity extends AppCompatActivity {
-
-    /*
-    //////
-    private ProgressBar mProgressBar; // Download or connection progress bar
-    private ImageView mLeftDevice; // Left device image
-    private ImageView mRightDevice; // Right device image
-    private ImageView mImageInfo; // Download or no bluetooth available image
-    private TextView mTextInfo; // Text info to display
-
-    //
-    */
-
-
-
-
 
     private void setDeviceAnimation(boolean right) {
 
@@ -83,295 +43,6 @@ public class ConnectActivity extends AppCompatActivity {
 
         Connectivity.getInstance().mListenDevice = !right;
     }
-    /*
-    private void displayError() { // Display no bluetooth UI components
-
-        mProgressBar.setVisibility(View.GONE);
-        mLeftDevice.setVisibility(View.GONE);
-        mRightDevice.setVisibility(View.GONE);
-
-        mTextInfo.setText(R.string.no_bluetooth);
-
-        mImageInfo.setImageDrawable(getResources().getDrawable(R.drawable.warning));
-        mImageInfo.setVisibility(View.VISIBLE);
-    }
-
-    ////// Download videos
-    private boolean mBluetoothAvailable; // Bluetooth available flag
-    private boolean mDownloading; // Downloading videos flag
-    private boolean mDownloaded; // Downloaded videos flag (persistent data)
-
-    private void displayDownload(boolean enable) { // Enable/Disable download videos UI components
-        if (enable) { // Enable download components
-
-            if (mBluetoothAvailable) {
-
-                mLeftDevice.clearAnimation();
-                mLeftDevice.setVisibility(View.GONE);
-                mRightDevice.clearAnimation();
-                mRightDevice.setVisibility(View.GONE);
-            }
-            mImageInfo.setVisibility(View.VISIBLE);
-            mImageInfo.setImageDrawable(getResources().getDrawable(R.drawable.download_anim));
-            ((AnimationDrawable)mImageInfo.getDrawable()).start();
-
-            mTextInfo.setText(getString(R.string.downloading_videos, "..."));
-
-            ((RelativeLayout.LayoutParams)mProgressBar.getLayoutParams()).height =
-                    getResources().getDimensionPixelSize(R.dimen.progress_height);
-            ((RelativeLayout.LayoutParams)mProgressBar.getLayoutParams()).width =
-                    ViewGroup.LayoutParams.MATCH_PARENT;
-            mProgressBar.setMax(1);
-            mProgressBar.setProgress(0);
-            mProgressBar.setIndeterminate(false);
-            mProgressBar.setVisibility(View.VISIBLE);
-        }
-        else { // Disable download components
-
-            if (!mBluetoothAvailable)
-                displayError();
-
-            else {
-
-                mLeftDevice.setVisibility(View.VISIBLE);
-                mRightDevice.setVisibility(View.VISIBLE);
-                mImageInfo.setVisibility(View.GONE);
-
-                setDeviceAnimation(!Connectivity.getInstance().mListenDevice);
-
-                mTextInfo.setText(getString(R.string.find_camera));
-
-                ((RelativeLayout.LayoutParams)mProgressBar.getLayoutParams()).height =
-                        ViewGroup.LayoutParams.WRAP_CONTENT;
-                ((RelativeLayout.LayoutParams)mProgressBar.getLayoutParams()).width =
-                        ViewGroup.LayoutParams.WRAP_CONTENT;
-                mProgressBar.setIndeterminate(true);
-            }
-        }
-        assert mMenuOptions.getItem(0).getItemId() == R.id.menu_album;
-        mMenuOptions.getItem(0).setEnabled(!enable);
-        assert mMenuOptions.getItem(1).getItemId() == R.id.menu_download;
-        mMenuOptions.getItem(1).setEnabled(!enable);
-    }
-
-    private DownloadVideosTask mDownloadTask;
-    private class DownloadVideosTask extends AsyncTask<Void, Integer, Integer> implements
-            Internet.OnDownloadListener {
-
-        private static final String WEBSERVICE_URL = "http://studio-artaban.com/Anaglyph3D/videos.php";
-
-        private static final String JSON_VIDEOS = "videos";
-        private static final String JSON_VIDEO = "video";
-        private static final String JSON_THUMBNAIL = "thumbnail";
-        private static final String JSON_URL = "url";
-        private static final String JSON_SIZE = "size";
-
-        //
-        private boolean mPublishProgress; // Publish progress flag
-        private Parcelable[] mDownloadedVideos; // Downloaded videos DB info
-
-        private int mTotalSize; // Total files size to download (in byte)
-        private short mTotalVideos; // Total videos to download
-        private short mCurVideo; // Current downloading video
-
-        private void getTotalSize(JSONArray list) throws JSONException {
-
-            mTotalVideos = 0;
-            mTotalSize = 0;
-            for (short i = 0; i < list.length(); ++i) {
-
-                // Add video file size
-                JSONObject video = list.getJSONObject(i).getJSONObject(JSON_VIDEO);
-                mTotalSize += video.getInt(JSON_SIZE);
-
-                // Add thumbnail file size
-                JSONObject thumbnail = list.getJSONObject(i).getJSONObject(JSON_THUMBNAIL);
-                mTotalSize += thumbnail.getInt(JSON_SIZE);
-
-                ++mTotalVideos;
-            }
-        }
-        private int getResultId(Internet.DownloadResult result) {
-
-            // Return error string ID or Constants.NO_DATA if succeeded
-            switch (result) {
-
-                case WRONG_URL:
-                case CONNECTION_FAILED:
-                    return R.string.webservice_unavailable;
-
-                case CANCELLED:
-                    return R.string.download_cancelled;
-            }
-            return Constants.NO_DATA; // No error
-        }
-
-        //////
-        @Override
-        protected Integer doInBackground(Void... params) {
-
-            // Empty downloads folder
-            Storage.removeTempFiles(true);
-
-            // Download JSON videos attributes under a web service
-            mPublishProgress = false;
-            int resultId = getResultId(Internet.downloadHttpFile(WEBSERVICE_URL,
-                    ActivityWrapper.DOCUMENTS_FOLDER + Storage.FOLDER_DOWNLOAD +
-                            Storage.FILENAME_DOWNLOAD_JSON, this));
-            if (resultId != Constants.NO_DATA)
-                return resultId; // Error or cancelled
-
-            try {
-
-                // Extract videos attributes from JSON file...
-                String attributes = Storage.readFile(new File(ActivityWrapper.DOCUMENTS_FOLDER +
-                    Storage.FOLDER_DOWNLOAD + Storage.FILENAME_DOWNLOAD_JSON));
-                JSONObject videos = new JSONObject(attributes);
-
-                // {
-                //     "videos": [
-                //         {
-                //             "video": {
-                //                 "url": "http://studio-artaban.com/Anaglyph3D/YYYY-MM-DD%20HH:MM:SS.000.webm",
-                //                 "size": 2000000
-                //             },
-                //             "thumbnail": {
-                //                 "url": "http://studio-artaban.com/Anaglyph3D/YYYY-MM-DD0%20HH:MM:SS.000.jpg",
-                //                 "size": 20000
-                //             },
-                //             "Album": {
-                //                 "title": "Titre",
-                //                 "description": "Description",
-                //                 "date": "YYYY-MM-DD HH:MM:SS.000",
-                //                 "duration": 30,
-                //                 "location": true,
-                //                 "latitude": 0.393489,
-                //                 "longitude": -22.104523,
-                //                 "thumbnailWidth": 640,
-                //                 "thumbnailHeight": 480
-                //             }
-                //         },
-                //
-                //         ...
-                //
-                //     ]
-                // }
-                JSONArray videoList = videos.getJSONArray(JSON_VIDEOS);
-
-                // Get total bytes to download
-                getTotalSize(videoList);
-                if ((mTotalSize == 0) || (mTotalVideos == 0))
-                    return R.string.wrong_videos_attr;
-
-                mCurVideo = 1;
-                mPublishProgress = true; // Ready to update progress bar
-                mDownloadedVideos = new Parcelable[mTotalVideos];
-
-                for (short i = 0; i < videoList.length(); ++i) {
-
-                    JSONObject album = videoList.getJSONObject(i).getJSONObject(AlbumTable.TABLE_NAME);
-                    String fileName = album.getString(AlbumTable.COLUMN_DATE); // Date is used as filename
-
-                    // Download video file
-                    JSONObject video = videoList.getJSONObject(i).getJSONObject(JSON_VIDEO);
-                    resultId = getResultId(Internet.downloadHttpFile(video.getString(JSON_URL),
-                            ActivityWrapper.DOCUMENTS_FOLDER + Storage.FOLDER_DOWNLOAD +
-                                    fileName + Constants.EXTENSION_WEBM, this));
-                    if (resultId != Constants.NO_DATA)
-                        return resultId; // Error or cancelled
-
-                    // Download thumbnail file
-                    JSONObject thumbnail = videoList.getJSONObject(i).getJSONObject(JSON_THUMBNAIL);
-                    resultId = getResultId(Internet.downloadHttpFile(thumbnail.getString(JSON_URL),
-                            ActivityWrapper.DOCUMENTS_FOLDER + Storage.FOLDER_DOWNLOAD +
-                                    fileName + Constants.EXTENSION_JPEG, this));
-                    if (resultId != Constants.NO_DATA)
-                        return resultId; // Error or cancelled
-
-                    // Fill videos DB info array
-                    SimpleDateFormat dateFormat = new SimpleDateFormat(Constants.DATABASE_DATE_FORMAT);
-                    mDownloadedVideos[i] = new AlbumTable.Video(0,
-                            album.getString(AlbumTable.COLUMN_TITLE), // Title
-                            album.getString(AlbumTable.COLUMN_DESCRIPTION), // Description
-                            dateFormat.parse(fileName), // Date
-                            (short)album.getInt(AlbumTable.COLUMN_DURATION), // Duration
-                            album.getBoolean(AlbumTable.COLUMN_LOCATION), // Location flag
-                            album.getDouble(AlbumTable.COLUMN_LATITUDE), // Latitude
-                            album.getDouble(AlbumTable.COLUMN_LONGITUDE), // Longitude
-                            album.getInt(AlbumTable.COLUMN_THUMBNAIL_WIDTH), // Thumbnail width
-                            album.getInt(AlbumTable.COLUMN_THUMBNAIL_HEIGHT)); // Thumbnail height
-
-                    ++mCurVideo;
-                }
-            }
-            catch (JSONException e) {
-
-                Logs.add(Logs.Type.F, "JSON Exception: " + e.getMessage());
-                return R.string.wrong_videos_attr;
-            }
-            catch (IOException e) {
-
-                Logs.add(Logs.Type.F, "IO Exception: " + e.getMessage());
-                return R.string.wrong_videos_attr;
-            }
-            catch (ParseException e) {
-
-                Logs.add(Logs.Type.F, "Parse exception: " + e.getMessage());
-                return R.string.wrong_videos_attr;
-            }
-            return Constants.NO_DATA; // Ok
-        }
-
-        @Override
-        protected void onPreExecute() {
-            mDownloading = true;
-            displayDownload(true);
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-
-            mProgressBar.setMax(mTotalSize);
-            mProgressBar.setProgress(mProgressBar.getProgress() + values[0]);
-
-            mTextInfo.setText(getString(R.string.downloading_videos, ": " +
-                    mCurVideo + "/" + mTotalVideos));
-        }
-
-        @Override
-        protected void onPostExecute(Integer result) {
-            mDownloading = false;
-            displayDownload(false);
-
-            if (result != Constants.NO_DATA) { // Display error message
-
-                DisplayMessage.getInstance().toast(result, Toast.LENGTH_LONG);
-                Connectivity.getInstance().start(ConnectActivity.this);
-            }
-            else {
-
-                mDownloaded = true;
-                assert mMenuOptions.getItem(1).getItemId() == R.id.menu_download;
-                mMenuOptions.getItem(1).setEnabled(false);
-
-                // Display videos album to add video entries into DB
-                Intent intent = new Intent(ConnectActivity.this, VideoListActivity.class);
-                intent.putExtra(AlbumActivity.DATA_VIDEOS_DOWNLOADED, mDownloadedVideos);
-
-                startActivityForResult(intent, 0);
-            }
-        }
-
-        //////
-        @Override public boolean onCheckCancelled() { return isCancelled(); }
-        @Override
-        public void onPublishProgress(int read) {
-
-            if (mPublishProgress)
-                publishProgress(read);
-        }
-    }
-    */
 
     //
     public void onLeftDeviceClick(View sender) { setDeviceAnimation(false); }
