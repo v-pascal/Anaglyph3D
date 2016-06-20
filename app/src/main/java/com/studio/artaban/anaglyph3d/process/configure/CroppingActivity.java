5f22b7887bb83738701e5c15c68bedeaf3f581a4
v-pascal <pascal.viguie@studio-artaban.com>
2016-06-20 23:33:01 +0200

    Replace synchro with cropping activity

--- app/src/main/java/com/studio/artaban/anaglyph3d/process/configure/CroppingActivity.java
+++ app/src/main/java/com/studio/artaban/anaglyph3d/process/configure/CroppingActivity.java
@@ -37,8 +37,8 @@ import java.nio.ByteBuffer;
 public class CroppingActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
     public static final String DATA_KEY_ZOOM = "zoom";
-    public static final String DATA_KEY_X = "x";
-    public static final String DATA_KEY_Y = "y";
+    public static final String DATA_KEY_ORIGIN_X = "x";
+    public static final String DATA_KEY_ORIGIN_Y = "y";
     public static final String DATA_KEY_LOCAL = "local";
 
     private static final String DATA_KEY_TRANS_X = "transX";
@@ -102,30 +102,8 @@ public class CroppingActivity extends AppCompatActivity implements SeekBar.OnSee
         Logs.add(Logs.Type.V, "seekBar: " + seekBar);
         mZoom = ((seekBar.getProgress() * (MAX_ZOOM - 1)) / 100f) + 1f;
 
-
-
-
-
-
-
-
-        int originX = (int)(mFrameWidth - (mZoom * mFrameWidth)) >> 1;
-        int originY = (int)(mFrameHeight - (mZoom * mFrameHeight)) >> 1;
-
-        mOriginX = ((originX + mTransX) < 0)? 0: originX + mTransX;
-        mOriginY = ((originY + mTransY) < 0)? 0: originY + mTransY;
-
-        displayZoom();
-
-
-
-
-
-
-
-
-
-
+        updateOrigin(0, 0);
+        updateZoom(false);
 
         if (!mChanged) {
 
@@ -140,8 +118,8 @@ public class CroppingActivity extends AppCompatActivity implements SeekBar.OnSee
 
         Intent intent = new Intent();
         intent.putExtra(DATA_KEY_ZOOM, mZoom);
-        intent.putExtra(DATA_KEY_X, mOriginX);
-        intent.putExtra(DATA_KEY_Y, mOriginY);
+        intent.putExtra(DATA_KEY_ORIGIN_X, mOriginX);
+        intent.putExtra(DATA_KEY_ORIGIN_Y, mOriginY);
         intent.putExtra(DATA_KEY_LOCAL, mLocal);
 
         setResult(RESULT_OK, intent);
@@ -153,6 +131,7 @@ public class CroppingActivity extends AppCompatActivity implements SeekBar.OnSee
         // Ask user to skip cropping step
         DisplayMessage.getInstance().alert(R.string.title_warning, R.string.ask_skip_step, null,
                 true, new DialogInterface.OnClickListener() {
+
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         if (which == DialogInterface.BUTTON_POSITIVE)
@@ -172,8 +151,7 @@ public class CroppingActivity extends AppCompatActivity implements SeekBar.OnSee
     private void resetChange(SeekBar seek) { // Cancel any changes (set to default values)
 
         Logs.add(Logs.Type.V, "seek: " + seek);
-        mCancelButton.setImageDrawable(getResources().getDrawable(
-                R.drawable.ic_invert_colors_white_48dp));
+        mCancelButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_invert_colors_white_48dp));
 
         // Reset cropping settings
         mZoom = DEFAULT_ZOOM;
@@ -182,8 +160,14 @@ public class CroppingActivity extends AppCompatActivity implements SeekBar.OnSee
         mLocal = DEFAULT_LOCAL;
 
         mChanged = false;
+
+        //
+        mTransX = mTransY = 0;
+        mPointerId = Constants.NO_DATA;
+        mPrevX = mPrevY = 0f;
+
         seek.setProgress(0);
-        displayZoom();
+        updateZoom(true);
     }
     private void getFrameImageSize(int parentHeight) {
         // Get frame image view size according frame and screen resolution and orientation
@@ -209,32 +193,19 @@ public class CroppingActivity extends AppCompatActivity implements SeekBar.OnSee
             mImageWidth = (int)(mFrameWidth * parentHeight / mFrameHeight);
         }
     }
-    private void displayZoom() { // Position images that define zoom configuration
+    private void updateZoom(boolean display) {
+        // Position images that show the zoom according current configuration
 
-        Logs.add(Logs.Type.V, null);
-
-
-
-        /*
+        //Logs.add(Logs.Type.V, null);
         final ImageView leftTop = (ImageView)findViewById((mLocal)? R.id.left_LT:R.id.right_LT);
         final ImageView rightTop = (ImageView)findViewById((mLocal)? R.id.left_RT:R.id.right_RT);
         final ImageView rightBottom = (ImageView)findViewById((mLocal)? R.id.left_RB:R.id.right_RB);
         final ImageView leftBottom = (ImageView)findViewById((mLocal)? R.id.left_LB:R.id.right_LB);
-        */
-        final ImageView leftTop = (ImageView)findViewById(R.id.left_LT);
-        final ImageView rightTop = (ImageView)findViewById(R.id.left_RT);
-        final ImageView rightBottom = (ImageView)findViewById(R.id.left_RB);
-        final ImageView leftBottom = (ImageView)findViewById(R.id.left_LB);
-
-
 
         assert leftTop != null;
         assert rightTop != null;
         assert rightBottom != null;
         assert leftBottom != null;
-
-
-
 
         int originX = (int)(mOriginX * mImageWidth / mFrameWidth);
         int originY = (int)(mOriginY * mImageHeight / mFrameHeight);
@@ -252,6 +223,158 @@ public class CroppingActivity extends AppCompatActivity implements SeekBar.OnSee
 
         leftBottom.setTranslationX(originX);
         leftBottom.setTranslationY(transY);
+
+        //
+        if (display)
+            displayZoom();
+    }
+    private void displayZoom() {
+        // Show or hide images according the frame on which to apply the zoom
+
+        Logs.add(Logs.Type.V, null);
+        if (mLocal) {
+
+            ImageView corner = (ImageView)findViewById(R.id.right_LT);
+            assert corner != null;
+            corner.setVisibility(View.GONE);
+            corner = (ImageView)findViewById(R.id.right_RT);
+            assert corner != null;
+            corner.setVisibility(View.GONE);
+            corner = (ImageView)findViewById(R.id.right_RB);
+            assert corner != null;
+            corner.setVisibility(View.GONE);
+            corner = (ImageView)findViewById(R.id.right_LB);
+            assert corner != null;
+            corner.setVisibility(View.GONE);
+
+            //
+            corner = (ImageView)findViewById(R.id.left_LT);
+            assert corner != null;
+            corner.setVisibility(View.VISIBLE);
+            corner = (ImageView)findViewById(R.id.left_RT);
+            assert corner != null;
+            corner.setVisibility(View.VISIBLE);
+            corner = (ImageView)findViewById(R.id.left_RB);
+            assert corner != null;
+            corner.setVisibility(View.VISIBLE);
+            corner = (ImageView)findViewById(R.id.left_LB);
+            assert corner != null;
+            corner.setVisibility(View.VISIBLE);
+        }
+        else {
+
+            ImageView corner = (ImageView)findViewById(R.id.left_LT);
+            assert corner != null;
+            corner.setVisibility(View.GONE);
+            corner = (ImageView)findViewById(R.id.left_RT);
+            assert corner != null;
+            corner.setVisibility(View.GONE);
+            corner = (ImageView)findViewById(R.id.left_RB);
+            assert corner != null;
+            corner.setVisibility(View.GONE);
+            corner = (ImageView)findViewById(R.id.left_LB);
+            assert corner != null;
+            corner.setVisibility(View.GONE);
+
+            //
+            corner = (ImageView)findViewById(R.id.right_LT);
+            assert corner != null;
+            corner.setVisibility(View.VISIBLE);
+            corner = (ImageView)findViewById(R.id.right_RT);
+            assert corner != null;
+            corner.setVisibility(View.VISIBLE);
+            corner = (ImageView)findViewById(R.id.right_RB);
+            assert corner != null;
+            corner.setVisibility(View.VISIBLE);
+            corner = (ImageView)findViewById(R.id.right_LB);
+            assert corner != null;
+            corner.setVisibility(View.VISIBLE);
+        }
+    }
+    private boolean moveOrigin(boolean local, MotionEvent event) {
+
+        //Logs.add(Logs.Type.V, "local: " + local + ", event: " + event);
+        switch (event.getActionMasked()) {
+
+            case MotionEvent.ACTION_DOWN: {
+                if (mPointerId == Constants.NO_DATA) {
+                    if (mLocal != local) {
+
+                        Logs.add(Logs.Type.I, "Change frame");
+                        mLocal = !mLocal;
+                        updateZoom(true);
+                    }
+                    else {
+
+                        Logs.add(Logs.Type.I, "Start changing origin");
+                        mPointerId = event.getPointerId(0);
+                        mPrevX = event.getX(0);
+                        mPrevY = event.getY(0);
+                    }
+                    return true;
+                }
+                break;
+            }
+            case MotionEvent.ACTION_UP: {
+                if (mPointerId == Constants.NO_DATA)
+                    break;
+
+                for (int i = 0; i < event.getPointerCount(); ++i) {
+                    if (event.getPointerId(i) == mPointerId) {
+
+                        Logs.add(Logs.Type.I, "Finish changing origin");
+                        mPointerId = Constants.NO_DATA;
+                        mPrevX = mPrevY = 0f;
+                        break;
+                    }
+                }
+                return true;
+            }
+            case MotionEvent.ACTION_MOVE: {
+                if (mPointerId == Constants.NO_DATA)
+                    break;
+
+                for (int i = 0; i < event.getPointerCount(); ++i) {
+                    if (event.getPointerId(i) == mPointerId) {
+
+                        updateOrigin((int)(event.getX(i) - mPrevX), (int)(event.getY(i) - mPrevY));
+                        updateZoom(false);
+                        // TODO: Add landscape natural orientation management
+
+                        mPrevX = event.getX(i);
+                        mPrevY = event.getY(i);
+                        break;
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+    private void updateOrigin(int deltaX, int deltaY) {
+        // Update origin values according new configuration
+
+        //Logs.add(Logs.Type.V, null);
+        int originX = (int)(mFrameWidth - (mZoom * mFrameWidth)) >> 1;
+        int originY = (int)(mFrameHeight - (mZoom * mFrameHeight)) >> 1;
+
+
+
+
+
+
+
+
+        if (((originX + mTransX + deltaX) >= 0) && ((originX + mTransX + deltaX) <= (originX << 1)))
+            mTransX += deltaX;
+        if (((originY + mTransY + deltaY) >= 0) && ((originY + mTransY + deltaY) <= (originY << 1)))
+            mTransY += deltaY;
+
+        mOriginX = ((originX + mTransX) < 0)? 0: (((originX + mTransX) > (originX << 1))? originX << 1: originX + mTransX);
+        mOriginY = ((originY + mTransY) < 0)? 0: (((originY + mTransY) > (originY << 1))? originY << 1: originY + mTransY);
+
+
+
 
 
 
@@ -287,8 +410,8 @@ public class CroppingActivity extends AppCompatActivity implements SeekBar.OnSee
         if (savedInstanceState != null) {
 
             mZoom = savedInstanceState.getFloat(DATA_KEY_ZOOM);
-            mOriginX = savedInstanceState.getInt(DATA_KEY_X);
-            mOriginY = savedInstanceState.getInt(DATA_KEY_Y);
+            mOriginX = savedInstanceState.getInt(DATA_KEY_ORIGIN_X);
+            mOriginY = savedInstanceState.getInt(DATA_KEY_ORIGIN_Y);
             mLocal = savedInstanceState.getBoolean(DATA_KEY_LOCAL);
 
             mTransX = savedInstanceState.getInt(DATA_KEY_TRANS_X);
@@ -414,16 +537,6 @@ public class CroppingActivity extends AppCompatActivity implements SeekBar.OnSee
             applyButton.setTranslationY(-(params.height >> 1));
         }
 
-
-
-
-
-
-
-
-
-
-
         // Load frame images
         final ImageView localFrame = (ImageView)findViewById(R.id.frame_left);
         final ImageView remoteFrame = (ImageView)findViewById(R.id.frame_right);
@@ -431,79 +544,13 @@ public class CroppingActivity extends AppCompatActivity implements SeekBar.OnSee
         assert localFrame != null;
         assert remoteFrame != null;
 
-
-
-        //localFrame.getTouchables().
-
-
-
         localFrame.setImageBitmap(openBitmapFile(true));
         localFrame.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
 
-                Logs.add(Logs.Type.V, "event: " + event);
-
-
-
-
-
-
-
-                /*
-                mTransX = -mOriginX;
-                mTransY = -mOriginY;
-                mOriginY = 0;
-                mOriginX = 0;
-                displayZoom();
-                */
-
-
-
-
-                switch (event.getActionMasked()) {
-
-                    case MotionEvent.ACTION_DOWN: {
-                        if (mPointerId == Constants.NO_DATA) {
-
-                            mPointerId = event.getPointerId(0);
-                            return true;
-                        }
-                        break;
-                    }
-                    case MotionEvent.ACTION_UP:
-
-                        // Always stop when any action up is found
-                        mPointerId = Constants.NO_DATA;
-                        break;
-
-                    case MotionEvent.ACTION_MOVE:
-                        if (mPointerId != Constants.NO_DATA) {
-                            for (int i = 0; i < event.getPointerCount(); ++i) {
-                                if (event.getPointerId(i) == mPointerId) {
-
-
-
-
-
-
-
-                                    event.getX(i);
-                                    event.getY(i);
-
-
-
-
-
-
-
-                                    break;
-                                }
-                            }
-                        }
-                        return true;
-                }
-                return false;
+                //Logs.add(Logs.Type.V, "event: " + event);
+                return moveOrigin(true, event);
             }
         });
         remoteFrame.setImageBitmap(openBitmapFile(false));
@@ -511,18 +558,8 @@ public class CroppingActivity extends AppCompatActivity implements SeekBar.OnSee
             @Override
             public boolean onTouch(View v, MotionEvent event) {
 
-
-
-
-
-
-
-
-
-
-
-
-                return false;
+                //Logs.add(Logs.Type.V, "event: " + event);
+                return moveOrigin(false, event);
             }
         });
 
@@ -533,25 +570,15 @@ public class CroppingActivity extends AppCompatActivity implements SeekBar.OnSee
         ((RelativeLayout.LayoutParams)localFrame.getLayoutParams()).width = mImageWidth;
         ((RelativeLayout.LayoutParams)remoteFrame.getLayoutParams()).width = mImageWidth;
 
-
-
-
-
-        displayZoom();
-
-
-
-
-
-
+        updateZoom(true);
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
 
         outState.putFloat(DATA_KEY_ZOOM, mZoom);
-        outState.putInt(DATA_KEY_X, mOriginX);
-        outState.putInt(DATA_KEY_Y, mOriginY);
+        outState.putInt(DATA_KEY_ORIGIN_X, mOriginX);
+        outState.putInt(DATA_KEY_ORIGIN_Y, mOriginY);
         outState.putBoolean(DATA_KEY_LOCAL, mLocal);
 
         outState.putInt(DATA_KEY_TRANS_X, mTransX);
