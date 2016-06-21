4ebcbbdc1f30d8ffd4c7e80f30937e124a944dff
v-pascal <pascal.viguie@studio-artaban.com>
2016-06-21 12:58:20 +0200

    Camera view fix

--- app/src/main/java/com/studio/artaban/anaglyph3d/process/RecorderFragment.java
+++ app/src/main/java/com/studio/artaban/anaglyph3d/process/RecorderFragment.java
@@ -211,6 +211,7 @@ public class RecorderFragment extends Fragment {
 
         // Create & Position preview camera surface
         mCameraView = new CameraView(getContext());
+        mCameraView.release();
 
         Point screenSize = new Point();
         getActivity().getWindowManager().getDefaultDisplay().getSize(screenSize);
